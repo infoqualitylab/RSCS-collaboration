345261e17a32c928469ea8945c1b4aba5d27df76
mvanmoer <mvanmoer@illinois.edu>
2022-02-24 17:47:13 -0600

    More or less replicates the ASIST sub-images but without annotations

--- backwards.py
+++ backwards.py
@@ -63,7 +63,12 @@ conditions = [
 choices = ['gold', 'lightskyblue', 'lightpink']
 nodesdf['fill'] = np.select(conditions, choices, default='black')
 
-# add labels
+# add node labels
+# first add a column where the id is a str
+nodesdf['labels'] = nodesdf['id'].astype('str')
+# now add 'SR' where appropriate
+nodesdf['labels'] = np.where(nodesdf.Type == 'Systematic Review', 'SR'+nodesdf.labels, nodesdf.labels)
+
 edgesdf = pd.merge(edgesdf, nodecoords, left_on='source', right_on='id')
 edgesdf = edgesdf.drop(['id'],axis=1)
 edgesdf = pd.merge(edgesdf, nodecoords, 
@@ -75,71 +80,94 @@ edgesdf = edgesdf.drop(['id'], axis=1)
 edgesdf['tuples'] = tuple(zip(edgesdf.source, edgesdf.target))
 
 # get the unique SR years to make cohorts
-SRs = nodesdf[nodesdf['Type'] == 'Systematic Review']
+#SRs = nodesdf[nodesdf['Type'] == 'Systematic Review']
 
 # loop over unique SR years grabbing just nodes <= y
 SRperiods = []
-for y in SRs['year'].unique():
+#uniquePeriods = SRs['year'].unique()
+uniquePeriods = nodesdf[nodesdf['Type'] == 'Systematic Review']['year'].unique()
+
+prev = uniquePeriods[0]
+for i, y in enumerate(uniquePeriods):
     nodes = nodesdf[nodesdf['year'] <= y]
-
     edges = edgesdf[edgesdf['source'].isin(nodes['id'])]
-    SRperiods.append({'nodes': nodes, 'edges': edges})
+    #nodes['outline'] = np.where(nodes.year < y, 'black', 'red')
 
+    SRperiods.append({'endyear': y, 'nodes': nodes, 'edges': edges})
 
-# drawing directly with matplotlib
-# issues with this approach
-# have to do too much work on things like scaling the arrows,
-# nodes, etc. 
-'''
-i = 0
-for period in SRperiods:
-
-    fig, ax = plt.subplots(figsize=(1280/96, 720/96), dpi=96)
-
-    # draw edges using patches
-    for idx, row in period['edges'].iterrows():
-        arrow = mpatches.FancyArrowPatch(row['source_coords'],
-                                         row['target_coords'],
-                                         mutation_scale=10,
-                                         arrowstyle='-|>',
-                                         color='gainsboro',
-                                         zorder=1)
-        ax.add_patch(arrow)
-
-    # draw the nodes
-    # color by attitude
-    plt.scatter(period['nodes']['x'], period['nodes']['y'],
-            s=150.0,
-            c=period['nodes']['fill'], zorder=2)
-
-    # draw the labels, just id for now
-    for idx, row in period['nodes'].iterrows():
-        plt.annotate(row['id'], (row['x'], row['y']), color='gray')
-    plt.axis('off')
-    plt.savefig('backwards-scatter-{}.png'.format(i))
-    plt.close()
-    i += 1
-'''
 
 # drawing with nx.draw_networkx_{nodes|edges}
 # this way requires that the subsets be dictionaries where the
 # keys are the 'id' and the values are the coordinate pairs 
-i = 0
-for period in SRperiods:
-    # this is ugly, if it works figure out a better storage
-    # we get back to the issue here that
-    # G.nodes is 1:14, 26:93
-    # so pd.DataFrame.to_dict() is not going to do what we want,
-    # however we can throw it into the dict ctor and be fine
-    nodepos = dict(period['nodes'][['id', 'coords']].values)
+for i, period in enumerate(SRperiods):
+    # TODO annoyingly nx.draw turns off padding, margins differently
+   
+    # SRs are split from PSRs because unlike coloring, shape can only
+    # be a single option per draw function.
+    SRs = period['nodes'][period['nodes']['Type'] == 'Systematic Review']
+    PSRs = period['nodes'][period['nodes']['Type'] == 'Primary Study Report']
+    SRnodepos = dict(SRs[['id', 'coords']].values)
+    PSRnodepos = dict(PSRs[['id', 'coords']].values)
 
-    # annoyingly nx.draw turns off padding, margins differently
-    nx.draw_networkx_nodes(G, nodepos, nodelist=nodepos.keys())
-    nx.draw_networkx_edges(G, nodepos, edgelist=period['edges']['tuples'].to_list())
+    # want to do an edgecolor on nodes that are new, whether SR or PSR,
+    # and no edgecolor on older nodes
+    # split edges based on if they're new this period, do this with
+    # a join on the previous period, i.e., old things are present 
+    # in the previous period. pandas doesn't have a direct anti-join, sigh...
+    # can't check on year because edges don't have years
+    nodepos = dict(period['nodes'][['id', 'coords']].values)
+    if i > 0:
+        current_nodes= period['nodes']
+        previous_nodes= SRperiods[i-1]['nodes']
+        tmp = current_nodes.merge(previous_nodes, how='outer', indicator=True)
+        new_nodes= tmp[tmp['_merge'] == 'left_only']
+        old_nodes= tmp[tmp['_merge'] == 'both']
+
+        new_SRs = new_nodes[new_nodes['Type'] == 'Systematic Review']
+        old_SRs = old_nodes[old_nodes['Type'] == 'Systematic Review']
+
+        new_PSRs = new_nodes[new_nodes['Type'] == 'Primary Study Report']
+        old_PSRs = old_nodes[old_nodes['Type'] == 'Primary Study Report']
+    
+        new_SRs_pos = dict(new_SRs[['id', 'coords']].values)
+        old_SRs_pos = dict(old_SRs[['id', 'coords']].values)
+
+        new_PSRs_pos = dict(new_PSRs[['id', 'coords']].values)
+        old_PSRs_pos = dict(old_PSRs[['id', 'coords']].values)
+
+        nx.draw_networkx_nodes(G, new_SRs_pos, nodelist=new_SRs_pos.keys(),
+            node_color=new_SRs['fill'].to_list(), node_shape='s', edgecolors='red')
+        nx.draw_networkx_nodes(G, old_SRs_pos, nodelist=old_SRs_pos.keys(),
+            node_color=old_SRs['fill'].to_list(), node_shape='s')
+        
+        nx.draw_networkx_nodes(G, new_PSRs_pos, nodelist=new_PSRs_pos.keys(),
+            node_color=new_PSRs['fill'].to_list(), edgecolors='red')
+        nx.draw_networkx_nodes(G, old_PSRs_pos, nodelist=old_PSRs_pos.keys(),
+            node_color=old_PSRs['fill'].to_list())
+        
+
+        current_edges = period['edges']
+        previous_edges = SRperiods[i-1]['edges']
+        tmp = current_edges.merge(previous_edges, how='outer', indicator=True)
+        new_edges = tmp[tmp['_merge'] == 'left_only']
+        old_edges = tmp[tmp['_merge'] == 'both']
+        
+        nx.draw_networkx_edges(G, nodepos, edgelist=old_edges['tuples'].to_list(), edge_color='darkgray')
+        nx.draw_networkx_edges(G, nodepos, edgelist=new_edges['tuples'].to_list(), edge_color='red')
+    else:
+        nx.draw_networkx_nodes(G, SRnodepos, nodelist=SRnodepos.keys(),
+                node_color=SRs['fill'].to_list(), node_shape='s')
+        nx.draw_networkx_nodes(G, PSRnodepos, nodelist=PSRnodepos.keys(),
+                node_color=PSRs['fill'].to_list())
+        nx.draw_networkx_edges(G, nodepos, period['edges']['tuples'].to_list(), edge_color='darkgray')
+
+    # labels are all drawn with the same style
+    # so no separation is necessary
+    nx.draw_networkx_labels(G, nodepos, labels = dict(period['nodes'][['id', 'labels']].values))
+    
 
     plt.axis('off')
     plt.tight_layout()
     plt.savefig('test-{}.png'.format(i), pad_inches=0, bbox_inches='tight')
     
     plt.close()
-    i+=1
