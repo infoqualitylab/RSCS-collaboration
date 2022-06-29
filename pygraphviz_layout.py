64728c5ad90ad36d189556992e4381110666d7ea
mvanmoer <mvanmoer@illinois.edu>
2022-06-29 15:21:12 -0500

    application that outputs images for all the meaningful pygraphviz engines

---
+++ pygraphviz_layout.py
@@ -0,0 +1,18 @@
+import InclusionNetwork
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='draw an inclusion network evolution over time')
+    parser.add_argument('cfgyaml', help='path to YAML config file')
+    args = parser.parse_args()
+
+    layouts = ['neato', 'dot', 'twopi', 'circo', 'fdp', 'sfdp']
+    for layout in layouts:
+        network = InclusionNetwork.InclusionNetwork(engine=layout)
+        network.load_cfgs(args.cfgyaml)
+        network.load_nodes()
+        network.load_edges()
+        network.create_graph()
+        network.layout_graph()
+        network.set_aesthetics()
+        network.draw_graph_evolution()
