efb8545422c9835ebfcebe6490014136441b0567
Mark Van Moer <mvanmoer@illinois.edu>
2022-09-14 09:32:04 -0500

    various attempts at aesthetics improvments

--- InclusionNetwork.py
+++ InclusionNetwork.py
@@ -23,16 +23,25 @@ class InclusionNetwork(IQLNetwork.IQLNetwork):
         self.edges = None
         self.Graph = None
         # Here's a thought question, what units are these in? Not pixels...
-        self.node_size = 100
+        self.node_size = 25
         self.edge_width = 0.5
         self.arrow_size = 5
         self.inconclusive_color = '#8da0cb'
         self.for_color = '#66c2a5'
         self.against_color = '#fc8d62'
         self.new_highlight = '#ff3333'
+        
         self.review_shape = 's'
         self.review_label = 'SR'
+        self.review_color = '#8fb1daff'
+        #self.review_edgecolor = '
+
         self.study_shape = 'o'
+        self.study_color = '#dddddd77'
+        self.study_edgecolor = '#b889c933'
+
+        self.edge_color = 'lightgray'
+
         self.engine = engine
         self._cfgs = {}
         # periods are subsets of the data based on when new 
@@ -71,11 +80,10 @@ class InclusionNetwork(IQLNetwork.IQLNetwork):
         # add fill colors
         try:
             conditions = [
-                self.nodes[self._cfgs['attitude']].eq('inconclusive'),
-                self.nodes[self._cfgs['attitude']].eq('for'),
-                self.nodes[self._cfgs['attitude']].eq('against')
+                self.nodes[self._cfgs['kind']].eq(self._cfgs['review']),
+                self.nodes[self._cfgs['kind']].eq(self._cfgs['study'])
             ]
-            choices = [self.inconclusive_color, self.for_color, self.against_color]
+            choices = [self.review_color, self.study_color]
             self.nodes['fill'] = np.select(conditions, choices, default='black')
         except KeyError:
             self.nodes['fill'] = 'lightgray'
@@ -174,6 +182,7 @@ class InclusionNetwork(IQLNetwork.IQLNetwork):
             fig, axs = plt.subplots()
 
         for i, period in enumerate(self.periods):
+            fig, axs = plt.subplots()
             # this tiles left-to-right, top-to-bottom
             
             if self._cfgs['tiled']:
@@ -201,9 +210,14 @@ class InclusionNetwork(IQLNetwork.IQLNetwork):
                 # split nodes on old vs new 
                 old_nodes, new_nodes = _split_old_new(i, period)
                 # reviews after studies and new after old so they're on top
-                _draw_sub_nodes(old_nodes, self._cfgs['study'], self.study_shape)
+                _draw_sub_nodes(old_nodes, self._cfgs['study'], self.study_shape, self.study_color)
                 _draw_sub_nodes(new_nodes, self._cfgs['study'], self.study_shape, self.new_highlight)
-                _draw_sub_nodes(old_nodes, self._cfgs['review'], self.review_shape)
+                PSRs = period['nodes'].loc[period['nodes']['our_item_type'] == 'includeditem']
+                PSRpos = dict(PSRs[[self._cfgs['id'],'coords']].values)
+                nx.draw_networkx_labels(self.Graph, PSRpos,
+                        labels = dict(PSRs[[self._cfgs['id'],'labels']].values),
+                        font_size=4, font_color='#1a1a1aaa')
+                _draw_sub_nodes(old_nodes, self._cfgs['review'], self.review_shape, self.review_color)
                 _draw_sub_nodes(new_nodes, self._cfgs['review'], self.review_shape, self.new_highlight)
 
                 # split edges on old vs new
@@ -211,24 +225,41 @@ class InclusionNetwork(IQLNetwork.IQLNetwork):
 
                 # MVM: wrap these calls?
                 nx.draw_networkx_edges(self.Graph, nodepos, edgelist=old_edges['tuples'].to_list(), 
-                        edge_color='darkgray', width=self.edge_width, arrowsize=self.arrow_size)
+                        edge_color=self.edge_color, width=self.edge_width, node_size=self.node_size, arrowsize=5)
                 
                 nx.draw_networkx_edges(self.Graph, nodepos, edgelist=new_edges['tuples'].to_list(), 
-                        edge_color=self.new_highlight, width=self.edge_width, arrowsize=self.arrow_size)
+                        edge_color=self.new_highlight, width=self.edge_width, node_size=self.node_size, arrowsize=5)
             else:
 
                 # first time through, don't split on old v. new
                 _draw_sub_nodes(period['nodes'], self._cfgs['study'], self.study_shape)
+                PSRs = period['nodes'].loc[period['nodes']['our_item_type'] == 'includeditem']
+                PSRpos = dict(PSRs[[self._cfgs['id'],'coords']].values)
+                nx.draw_networkx_labels(self.Graph, PSRpos,
+                        labels = dict(PSRs[[self._cfgs['id'],'labels']].values),
+                        font_size=4, font_color='#1a1a1aaa')
                 _draw_sub_nodes(period['nodes'], self._cfgs['review'], self.review_shape)
 
                 nx.draw_networkx_edges(self.Graph, nodepos, period['edges']['tuples'].to_list(), 
-                        edge_color='darkgray', width=self.edge_width, arrowsize=self.arrow_size)
+                        edge_color=self.edge_color, width=self.edge_width, node_size=self.node_size, arrowsize=5)
         
             # labels are all drawn with the same style regardles of node kind 
             # so no separation is necessary
+            '''
             nx.draw_networkx_labels(self.Graph, nodepos, 
                     labels = dict(period['nodes'][[self._cfgs['id'], 'labels']].values),
                     font_size=6, font_color='#1a1a1a')
+
+            '''
+
+            SRs = period['nodes'].loc[period['nodes']['our_item_type'] == 'reviewarticle']
+            SRpos = dict(SRs[[self._cfgs['id'],'coords']].values)
+            nx.draw_networkx_labels(self.Graph, SRpos,
+                    labels = dict(SRs[[self._cfgs['id'],'labels']].values),
+                    font_size=6, font_color='#1a1a1a')
+
+            
+
             plt.axis('off')
 
             # if odd number of subplots, don't draw axes around an empty last plot
@@ -237,9 +268,14 @@ class InclusionNetwork(IQLNetwork.IQLNetwork):
             plt.tight_layout()
             if not self._cfgs['tiled']:
                 plt.savefig('{}-inclusion-net-{}-{}.png'.format(self._cfgs['collection'],self.engine, i), dpi=300)
+
+            plt.clf()
+            #plt.cla()
+            #plt.close()
+
         if self._cfgs['tiled']:
             plt.savefig('{}-tiled-inclusion-net-{}.png'.format(self._cfgs['collection'],self.engine), dpi=300)
-        
+
 
 if __name__ == '__main__':
    
